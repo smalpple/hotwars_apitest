1fd557e81d936935b8e10b76a7fd6b67d3ff91ee
smallapple_by <biyong199458@163.com>
2020-08-25 17:32:32 +0800

    init project

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,4 +1,8 @@
-from hogwarts_apitest import __version__
+from hogwarts_apitest.number import __version__,__author__
 
 def test_version():
+
     assert isinstance(__version__,str)
+
+def test_author():
+    assert isinstance(__author__,str)
