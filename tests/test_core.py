59389c755ca18a7732a6a057c8980e35f0bbce3d
smallapple_by <biyong199458@163.com>
2020-09-25 14:14:14 +0800

    init project

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,8 +1,15 @@
 from hogwarts_apitest.number import __version__,__author__
+import requests
 
 def test_version():
-
     assert isinstance(__version__,str)
 
 def test_author():
     assert isinstance(__author__,str)
+
+def test_httpbin_get():
+    res = requests.get("https://httpbin.org/get",
+                       headers={"accept":"application/json"})
+
+    assert res.status_code == 200
+    assert res.headers["server"] == "gunicorn/19.9.0"
