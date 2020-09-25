bd34149522e1b29b9ef599f13c9d1c69b599e1cf
smallapple_by <biyong199458@163.com>
2020-09-25 16:49:54 +0800

    init project

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,5 +1,34 @@
 from hogwarts_apitest.number import __version__,__author__
 import requests
+import json
+
+class ApiHttpBinGet:
+    url = "https://httpbin.org/get"
+    method = "GET"
+    headers = {"accept":"application/json"}
+
+    def run(self,params=None):
+        res = requests.get(url=self.url,
+                       headers=self.headers,
+                       params=params)
+        self.res = res
+        return self
+
+    def validate(self,key,expected_value):
+        if "." in key:
+            key_first,key_last = key.split(".")[0],key.split(".")[-1]
+            actual_value = json.loads(
+                json.dumps(
+                dict(
+                getattr(self.res,key_first))
+                )
+            )[key_last]
+        else:
+            actual_value = getattr(self.res,key)
+            print(actual_value)
+        assert actual_value == expected_value
+        return self
+
 
 def test_version():
     assert isinstance(__version__,str)
@@ -8,8 +37,9 @@ def test_author():
     assert isinstance(__author__,str)
 
 def test_httpbin_get():
-    res = requests.get("https://httpbin.org/get",
-                       headers={"accept":"application/json"})
+    ApiHttpBinGet().run(params={"abc":123})\
+        .validate('status_code',200)\
+        .validate('headers.Server',"gunicorn/19.9.0")\
+        # .validate("json","https://httpbin.org/get?abc=123")
 
-    assert res.status_code == 200
-    assert res.headers["server"] == "gunicorn/19.9.0"
+
